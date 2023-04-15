626a578425e129d9dd78bb91f045b526b19e445b
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-04-15 17:17:39 -0300

    first commit

---
+++ src/class/Hero.ts
@@ -0,0 +1,26 @@
+import powers from "../data/powers"
+
+export class Hero {
+
+
+    constructor(
+
+        public name: string,
+        public powerId: number,
+        public age: number
+        
+    ) {
+
+    }
+
+    get power(): string {
+        return powers.find(power => power.id === this.powerId)?.desc || 'no se encontro poder'
+    }
+}
+
+export class Hero2 { }
+export class Hero3 { }
+export class Hero4 { }
+
+export const PI = 3.1416
+export const nombre = 'Ivan'
---
+++ src/data/powers.ts
@@ -0,0 +1,16 @@
+export interface Powers {
+    id: number,
+    desc: string
+}
+
+ const powers: Powers[] = [
+
+    {
+        id: 1, desc: 'money'
+    },
+    {
+        id: 2, desc: 'energy'
+    }
+    
+]
+export default powers;
